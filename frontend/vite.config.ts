f4ec435c39b5b7cea7c16ba100b910f509da7916
yagyesh_bobde <yagyeshsrbobde@gmail.com>
2024-07-04 22:15:00 +0530

    updated package, build script, config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,20 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'THIS_IS_UNDEFINED') return
+        warn(warning)
+      },
+    },
   }
 })
